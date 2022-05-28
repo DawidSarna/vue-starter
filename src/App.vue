26839b8414cb8fbdf4fb7c622bc614f97ae4b457
student21 <student21@pcoip.ki.agh.edu.pl>
2022-05-28 14:02:43 +0200

    email length

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div>
   
 <h1>Twój e-mail to: {{ email }}</h1>
+<div v-if="email.length == 0">podaj swoj e-mail</div>
+<div v-else-if="email.length < 10 & email.length > 0">Ale masz krótki adres!</div>
+<div v-else-if="email.length < 20">Twój adres e-mail jest w sam raz.</div>
+<div v-else id="redColor" >Twój adres e-mail jest stanowczo za długi!</div>
 <input type="email" v-model="email">
 
   </div>
@@ -19,5 +23,7 @@ data() {
 </script>
 
 <style>
-
+ #redColor {
+	color: red;
+ }
 </style>
