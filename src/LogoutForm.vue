47a6e6ca0f8813ac181bdfcc4dc6ff94a44485a7
student21 <student21@pcoip.ki.agh.edu.pl>
2022-05-28 15:59:09 +0200

    komponent logout full

---
+++ src/LogoutForm.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+	<h3>Witaj {{ email }}</h3>
+	<button @click="logout()">Wyloguj</button>
+   </div>
+</template>
+
+<script>
+export default {
+ props: ['email'],
+
+ data() {
+     return {
+         email: '',
+     }
+ },
+ methods: {
+  logout() {
+  this.$emit('logout');
+  }
+ }
+}
+</script>
